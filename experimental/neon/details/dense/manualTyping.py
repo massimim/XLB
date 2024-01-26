0061a24cf20cc6437b50e28e60214003f4d68ea2
Massimiliano Meneghin <massimiliano.meneghin@autodesk.com>
2024-01-26 18:29:10 +0100

    kernel running

--- experimental/neon/details/dense/manualTyping.py
+++ experimental/neon/details/dense/manualTyping.py
@@ -68,6 +68,28 @@ class IdxDenseGrid:
         return self.i[2]
 
 
+@wp.func
+def IdxDenseGrid_method_set(self: IdxDenseGrid, x: int, y: int, z: int):
+    self.i[0] = x
+    self.i[1] = y
+    self.i[2] = z
+
+
+@wp.func
+def IdxDenseGrid_method_get_x(self: IdxDenseGrid):
+    return self.i[0]
+
+
+@wp.func
+def IdxDenseGrid_method_get_y(self: IdxDenseGrid):
+    return self.i[1]
+
+
+@wp.func
+def IdxDenseGrid_method_get_z(self: IdxDenseGrid):
+    return self.i[2]
+
+
 @wp.struct
 class SpanDenseGrid:
     dim: wp.vec3i
@@ -91,6 +113,9 @@ class SpanDenseGrid:
     def get_ghost_radius(self):
         return self.ghost_radius
 
+    def get_dim(self):
+        return self.dim
+
     def set_idx(self, x: int, y: int, z: int, idx: IdxDenseGrid):
         idx.set(x, y, z + self.ghost_radius)
 
@@ -105,9 +130,7 @@ def set_span(span: SpanDenseGrid, x: int, y: int, z: int):
 
 @wp.func
 def SpanDenseGrid_method_set_idx(span: SpanDenseGrid, x: int, y: int, z: int, idx: IdxDenseGrid):
-    idx.x = x
-    idx.y = y
-    idx.z = z + span.ghost_radius
+    IdxDenseGrid_method_set(idx, x, y, z + span.ghost_radius)
 
 
 @wp.struct
@@ -132,9 +155,9 @@ class PartitionDenseGrid_int:
 @wp.func
 def partition_int_operator_get(p: PartitionDenseGrid_int, grid_idx: IdxDenseGrid, card: int):
     pitch = (
-            grid_idx.get_x() +
-            p.pitch[0] * grid_idx.get_y() +
-            p.pitch[1] * grid_idx.get_z() +
+            IdxDenseGrid_method_get_x(grid_idx) +
+            p.pitch[0] * IdxDenseGrid_method_get_y(grid_idx) +
+            p.pitch[1] * IdxDenseGrid_method_get_z(grid_idx) +
             p.pitch[2] * card)
     return p.memory[pitch]
 
@@ -188,21 +211,22 @@ class FieldDenseGrid_int:
         bk_host.for_each_device(set_partition(self.host_partition_table, bk_host))
         self.bk.for_each_device(set_partition(self.host_partition_table, self.bk))
 
+    def get_partition(self, data_view: str, device_idx: int) -> PartitionDenseGrid_int:
+        return self.device_partition_table[data_view][device_idx]
 
-def updateHost(self, stream: int):
-    """
-    Move data from the host to the device buffer asynchronously.
-    """
-    print("TODO")
-    pass
+    def updateHost(self, stream: int):
+        """
+        Move data from the host to the device buffer asynchronously.
+        """
+        print("TODO")
+        pass
 
-
-def updateDevice(self, stream: int):
-    """
-    Move data from the device to the host buffer asynchronously.
-    """
-    print("TODO")
-    pass
+    def updateDevice(self, stream: int):
+        """
+        Move data from the device to the host buffer asynchronously.
+        """
+        print("TODO")
+        pass
 
 
 class GridDense:
@@ -238,9 +262,72 @@ class GridDense:
         self.bk.for_each_device(set_span)
         pass
 
+    def get_span(self, data_view: str, device_idx: int) -> SpanDenseGrid:
+        return self.span_table[data_view][device_idx]
+
     def new_int_field(self, card: int):
         fied = FieldDenseGrid_int(self.bk, self.span_table, card, self.ghost_radius)
         return fied
+
+    def get_backend(self):
+        return self.bk
+
+
+class Container:
+    def __init__(self,
+                 wp_kernel,
+                 grid: GridDense,
+                 input_set,
+                 host_or_device):
+
+        self.kernel = wp_kernel
+        self.grid = grid
+        self.input_set = input_set
+        self.bk = grid.get_backend()
+
+        if host_or_device == 'host':
+            raise Exception(f"Work in progress")
+
+    def run(self, stream: int):
+        if self.bk.type == 'cpu':
+            # error, through exception
+            raise Exception(f"Work in progress")
+            pass
+        if self.bk.type == 'cuda':
+            def run_kernel(idx, device_code):
+                span = self.grid.get_span('standard', idx)
+                input = []
+                for i in self.input_set:
+                    input.append(i.get_partition('standard', idx))
+                print("wp.launch")
+                wp.launch(kernel=self.kernel,
+                          dim=span.get_dim(),
+                          inputs=[span] + input,
+                          device=device_code)
+                wp.synchronize()
+
+            self.bk.for_each_device(run_kernel)
+
+
+def get_kernel_add_one(grid: GridDense,
+                       fieldA: FieldDenseGrid_int):
+    @wp.kernel
+    def kernel_add_one(span: SpanDenseGrid,
+                       partition: PartitionDenseGrid_int):
+        x, y, z = wp.tid()
+        print("kernel_add_one")
+        g_idx = IdxDenseGrid()
+        SpanDenseGrid_method_set_idx(span, x, y, z, g_idx)
+        val = partition_int_operator_get(partition, g_idx, 0)
+        val = val + 1
+        pass
+
+    c = Container(wp_kernel=kernel_add_one,
+                  grid=grid,
+                  input_set=[fieldA],
+                  host_or_device='device')
+
+    return c
 
 
 def main():
@@ -256,8 +343,11 @@ def main():
 
     bk = Backend('cuda', devices)
     grid = GridDense(dim=dim, backend=bk, ghost_radius=1)
-    int_field = grid.new_int_field(card=1)
-    print(f"int_field {int_field}")
+    fieldA = grid.new_int_field(card=1)
+    print(f"int_field {fieldA}")
+
+    c = get_kernel_add_one(grid, fieldA)
+    c.run(0)
 
 
 if __name__ == '__main__':
